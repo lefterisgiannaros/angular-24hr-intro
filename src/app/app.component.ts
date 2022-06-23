94081818edb80cdbdb2f1e38261c17f6a204d359
Lefteris Giannaros <lefterisgiannaros@gmail.com>
2022-06-23 19:11:04 +0300

    break

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-24h-lemans';
+
+  // HOOKS
   intervalSub;
 
+  // this is a hook that's used on init. logs an interval.
   ngOnInit(){
     this.intervalSub = setInterval(() => {
       console.log('Hello from ngOnInit');
     }, 1000);
   }
-
+// this is the hook that's used before destroying smth. We clear the interval.
   ngOnDestroy() {
     if (this.intervalSub) {
       clearInterval(this.intervalSub);
