e12e138544399fe7c9395480ddfb971b1825d98c
Lefteris Giannaros <lefterisgiannaros@gmail.com>
2022-06-23 18:11:32 +0300

    yup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-24h-lemans';
+  intervalSub;
+
+  ngOnInit(){
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+
 }
